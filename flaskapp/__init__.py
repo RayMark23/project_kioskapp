2855024ded04dc1feacb7c331b03857fc427116d
raymark23 <raymarkrockz23@gmail.com>
2024-01-18 12:55:36 +0800

    new update

--- flaskapp/__init__.py
+++ flaskapp/__init__.py
@@ -11,3 +11,7 @@ app.config['MYSQL_DB'] = 'raymark_kiosk'
 mysql = MySQL(app)
 
 from flaskapp import routes
+
+app.config.update(
+    TESTING=True,
+    SECRET_KEY='192b9bdd22ab9ed4d12e236c78afcb9a393ec15f71bbf5dc987d54727823bcbf')
